f2fa5244aa951a11a40ec313282b2bb986b70059
Naomi Dushay <ndushay@stanford.edu>
2012-02-24 13:19:09 -0800

    extracting ckey while reading row

--- lib/parse_crez_data.rb
+++ lib/parse_crez_data.rb
@@ -13,13 +13,21 @@ class ParseCrezData
 #  MUSIC-RESV|20120109|CURRENT|555|36105041861316  |SCORES|MUSIC-RESV|ON_RESERVE|4H|20120109|ACTIVE|MUSIC-122C||FAL-WIN-SP|2000171265|09780727|Ferneyhough, Brian|
   
   
+  # from parsing, want:
+  #   ckey =>  array of on-rez items
+  #     each item has all the stuff from the line
+  
+  attr :ckey_to_item_crez_info
+  
   
   # NAOMI_MUST_COMMENT_THIS_METHOD
   def read(csv_file_path)
     
-    CSV.foreach(File.expand_path(csv_file_path, File.dirname(__FILE__))) do |row|
-#    CSV.foreach(File.expand_path('../spec/test_data/nonrezmiddle.csv', File.dirname(__FILE__))) do |row|
-
+    CSV.foreach(File.expand_path(csv_file_path, File.dirname(__FILE__)), {:col_sep => '|'}) do |row|
+#      puts row
+#      puts row[0]
+      ckey = row[3]
+      puts ckey
 
 #    CSV.new(:headers => @csv_cols, :header_converters => :symbol)
       # row is an Array of fields
@@ -28,7 +36,6 @@ class ParseCrezData
       # 
       # otherwise, make a struct with all the relevant data and put it in the hash
       # use row here...
-      puts row
     end
   end
   
