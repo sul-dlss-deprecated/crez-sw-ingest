cda159d8bcf34657e1d221881b23f55dcc8015d3
Naomi Dushay <ndushay@stanford.edu>
2012-02-29 11:55:01 -0800

    improvements on the hardcoding of solrmarc_dist_dir

--- spec/get_solrdoc_from_solrmarc_spec.rb
+++ spec/get_solrdoc_from_solrmarc_spec.rb
@@ -2,8 +2,15 @@ require 'get_solrdoc_from_solrmarc'
 
 describe GetSolrdocFromSolrmarc do
   
+# FIXME:  need a way to avoid hardcoding the solrmarc directory  
+  
+  before(:all) do
+    @@solrmarc_dist_dir = "/hudson/home/hudson/hudson/jobs/solrmarc-SW-solr3.5-dist/workspace/dist"
+#    @@solrmarc_dist_dir = "/Users/ndushay/searchworks/solrmarc-sw/dist"
+  end
+  
   it "should retrieve the SolrInputDoc generated from the marc record" do
-    m = GetSolrdocFromSolrmarc.new("/hudson/home/hudson/hudson/jobs/solrmarc-SW-solr3.5-dist/workspace/dist", "sw_config.properties")
+    m = GetSolrdocFromSolrmarc.new(@@solrmarc_dist_dir, "sw_config.properties")
     d = m.get_solr_input_doc("666")
     d.should be_an_instance_of(Java::OrgApacheSolrCommon::SolrInputDocument)
     d["id"].getValue.should == "666"
@@ -11,7 +18,7 @@ describe GetSolrdocFromSolrmarc do
   end
   
   it "should have a SolrInputDoc with the non-stored fields present" do
-    m = GetSolrdocFromSolrmarc.new("/hudson/home/hudson/hudson/jobs/solrmarc-SW-solr3.5-dist/workspace/dist", "sw_config.properties")
+    m = GetSolrdocFromSolrmarc.new(@@solrmarc_dist_dir, "sw_config.properties")
     d = m.get_solr_input_doc("666")
     d["title_245a_search"].getValue.should_not be_nil
   end
