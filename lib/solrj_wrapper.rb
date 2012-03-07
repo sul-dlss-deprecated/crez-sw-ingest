4d313f9b9a4af6ecb57adfad8b37b9b36c87458f
Naomi Dushay <ndushay@stanford.edu>
2012-03-07 15:57:08 -0800

    removed vestigial, commented out code

--- lib/solrj_wrapper.rb
+++ lib/solrj_wrapper.rb
@@ -14,8 +14,6 @@ class SolrjWrapper
       raise "SolrjWrapper only runs under jruby"
     end
     load_solrj(solrj_jar_dir)
-#    # the full path for the config/solr.yml file
-#    @solr_config_file = File.expand_path('../config/solr.yml', File.dirname(__FILE__))
     @streaming_update_server = streaming_update_server(solr_url, queue_size, num_threads)
     useJavabin!
   end
@@ -67,16 +65,5 @@ class SolrjWrapper
   def load_solrj(solrj_jar_dir)
     Dir["#{solrj_jar_dir}/*.jar"].each {|jar_file| require jar_file }
   end
-=begin  
-# FIXME:  duplicated in solrmarc_wrapper ...  
-  # set solr_url to value of "url" in config/solr.yml`
-  def solr_url
-    @solr_url ||= begin
-      raise "You are missing the config/solr.yml file: #{@solr_config_file}. " unless File.exists?(@solr_config_file) 
-      @solr_config = YAML::load(File.open(@solr_config_file))
-      raise "config/solr.yml must have a value for 'url'" unless @solr_config["url"] 
-      @solr_config["url"]
-    end
-  end
-=end  
+
 end
