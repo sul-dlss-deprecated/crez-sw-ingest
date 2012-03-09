304eafe903c321a125599d3efae275155c46a9f6
Naomi Dushay <ndushay@stanford.edu>
2012-03-09 12:22:00 -0800

    working on class and script to tie it all together - it's not working yet

--- Gemfile
+++ Gemfile
@@ -3,16 +3,10 @@ source :rubygems
 gem 'yard' 				# for javadoc-y documentation tags
 gem 'RedCloth' 		# for textile formatting used in Readme
 gem 'rake'
-#gem 'rsolr'
 
 group :test do
 	gem 'rspec'
 	gem 'simplecov', :require => false
 	gem 'simplecov-rcov', :require => false
-#	gem 'marc'  # test that marcxml returned is parseable
-end
-
-group :development do
-  gem 'ripl'
-#	gem 'ruby-debug19', :require => 'ruby-debug'  # doesn't play well with jruby
+	gem 'rsolr'  # test that solr index updates are done correctly
 end
---
+++ lib/index_crez_data.rb
@@ -0,0 +1,47 @@
+require 'parse_crez_data'
+require 'solrj_wrapper'
+require 'add_crez_to_solr_doc'
+
+# Use this class to take a csv file containing course reserve data and add the appropriate information to the 
+# appropriate Solr Documents.
+class IndexCrezData
+  
+  # Given the full path to a file containing course reserve data, and all the parameter values for indexing,
+  #   add the course reserve data to the  create a Hash ckey => crez_info data
+  # @param crez_data_file  full path to the course reserve data file, a csv file created from Sirsi Symphony by a script written by Darsi
+  # @param solrmarc_wrapper  SolrmarcWrapper object for accessing SolrMarc 
+  # @param solrj_wrapper  SolrjWrapper for using SolrJ objects
+  def index_crez_data(crez_data_file, solrmarc_wrapper, solrj_wrapper)
+    ckey_2_crez_info = get_ckey_2_crez_info(crez_data_file)
+    sus = solrj_wrapper.streaming_update_server
+    ac2sd = AddCrezToSolrDoc.new(ckey_2_crez_info, solrmarc_wrapper, solrj_wrapper)
+    ckey_2_crez_info.keys.each { |ckey|  
+      solr_input_doc = ac2sd.add_crez_info_to_solr_doc(ckey)
+      begin
+        sus.add(solr_input_doc)
+      rescue org.apache.solr.common.SolrException => e 
+        logger.error("SolrException while indexing document #{ckey}")
+        logger.error("#{e.printStackTrace}")
+      end
+    }
+    begin
+      sus.commit
+    rescue org.apache.solr.common.SolrException => e
+      logger.error("SolrException while committing updates")
+      logger.error("#{e.printStackTrace}")
+    end
+    
+  end
+  
+protected  
+  
+  # Given the full path to a file containing course reserve data, create a Hash ckey => crez_info data
+  # @param crez_data_file  full path to the course reserve data file, a csv file created from Sirsi Symphony by a script written by Darsi
+  # @return Hash with key of ckey, value an Array of CSV::Row objects each containing data pertaining to a specific item (barcode) associated with the ckey
+  def get_ckey_2_crez_info(crez_data_file)
+    p = ParseCrezData.new
+    p.read(crez_data_file)
+    p.ckey_2_crez_info
+  end
+    
+end
