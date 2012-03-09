01e6aa410e11baf6a99030d8f90ebc7d993016b6
Naomi Dushay <ndushay@stanford.edu>
2012-03-08 17:28:56 -0800

    renamed yml to settings.yml

--- lib/settings.rb
+++ lib/settings.rb
@@ -5,14 +5,14 @@ class Settings
   
   attr_reader :solrmarc_dist_dir, :solrmarc_conf_props_file, :solr_url, :solrj_jar_dir, :solrj_queue_size, :solrj_num_threads
   
-  def initialize(env)
-    config = YAML.load_file('config/crez_sw_ingest.yml')[env]
-    @solrmarc_dist_dir = config["solrmarc_dist_dir"]
-    @solrmarc_conf_props_file = config["solrmarc_conf_props_file"]
-    @solr_url = config["solr_url"]
-    @solrj_jar_dir = config["solrj_jar_dir"]
-    @solrj_queue_size = config["solrj_queue_size"]
-    @solrj_num_threads = config["solrj_num_threads"]
+  def initialize(settings_group)
+    yml = YAML.load_file('config/settings.yml')[settings_group]
+    @solrmarc_dist_dir = yml["solrmarc_dist_dir"]
+    @solrmarc_conf_props_file = yml["solrmarc_conf_props_file"]
+    @solr_url = yml["solr_url"]
+    @solrj_jar_dir = yml["solrj_jar_dir"]
+    @solrj_queue_size = yml["solrj_queue_size"]
+    @solrj_num_threads = yml["solrj_num_threads"]
   end
   
 end
