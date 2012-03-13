00d82a40ab061bb2749d424c68ab78512136e7c1
Naomi Dushay <ndushay@stanford.edu>
2012-03-13 00:46:26 -0700

    be more robust parsing the raw data and print out error messages for unparseable lines

--- lib/parse_crez_data.rb
+++ lib/parse_crez_data.rb
@@ -1,4 +1,5 @@
-require 'csv' 
+require 'csv'
+require 'logger'
 
 # Read a CSV file containing Course Reserve data dumped from Sirsi, per spec here:
 #   https://consul.stanford.edu/display/NGDE/Symphony+Course+Reserves+Data+spec
@@ -11,18 +12,29 @@ class ParseCrezData
   @@csv_cols = "rez_desk|resctl_exp_date|resctl_status|ckey|barcode|home_loc|curr_loc|item_rez_status|loan_period|rez_expire_date|rez_stage|course_id|course_name|term|instructor_name"
   
   def initialize
+# FIXME:  need to log to a file, passed in
+    @logger = Logger.new(STDERR)
     @ckey_2_crez_info ||= {}
   end
   
   # read the csv file and populate @ckey_2_crez_info
   def read(csv_file_path)
-    csv_options = {:col_sep => '|', :headers => @@csv_cols, :header_converters => :symbol}
+    # re :quote_char - setting it to something other than "
+    #   see http://stackoverflow.com/questions/8073920/importing-csv-ruby-1-9-2-quoting-error-driving-me-nuts 
+    csv_options = {:col_sep => '|', :headers => @@csv_cols, :header_converters => :symbol, :quote_char => "\x00"}
     
-    CSV.foreach(File.expand_path(csv_file_path, File.dirname(__FILE__)), csv_options) do |row|
-      if row[:item_rez_status] == "ON_RESERVE"
-        ckey = row[:ckey]
-        crez_value = @ckey_2_crez_info[ckey] || []
-        @ckey_2_crez_info[ckey] = crez_value << row
+    File.open(csv_file_path, 'r').each do |line|
+      begin
+        CSV.parse(line, csv_options) do |row|
+          if row[:item_rez_status] == "ON_RESERVE"
+            ckey = row[:ckey]
+            crez_value = @ckey_2_crez_info[ckey] || []
+            @ckey_2_crez_info[ckey] = crez_value << row
+          end
+        end
+      rescue CSV::MalformedCSVError => e
+        @logger.error("CSV::MalformedCSVError while parsing #{csv_file_path} -- #{e.message} -- #{line}")
+        next
       end
     end
   end
