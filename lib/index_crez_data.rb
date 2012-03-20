1bdd112ea28257d3b4aab2b5c119deb289957dca
Naomi Dushay <ndushay@stanford.edu>
2012-03-19 17:30:49 -0700

    major refactor of index_crez_data; it also now removes old crez data as necessary

--- lib/index_crez_data.rb
+++ lib/index_crez_data.rb
@@ -7,41 +7,34 @@ require 'logger'
 # appropriate Solr Documents.
 class IndexCrezData
   
-  # Given the full path to a file containing course reserve data, and all the parameter values for indexing,
-  #   add the course reserve data to the  create a Hash ckey => crez_info data
-  # @param crez_data_file  full path to the course reserve data file, a csv file created from Sirsi Symphony by a script written by Darsi
   # @param solrmarc_wrapper  SolrmarcWrapper object for accessing SolrMarc 
   # @param solrj_wrapper  SolrjWrapper for using SolrJ objects
-  def index_crez_data(crez_data_file, solrmarc_wrapper, solrj_wrapper)
-# FIXME:  need to log to a file, passed in
-    @logger = Logger.new(STDERR)
+  def initialize(solrmarc_wrapper, solrj_wrapper, log_file=STDERR)
+    @logger = Logger.new(log_file)
+    @solrmarc_wrapper = solrmarc_wrapper
+    @solrj_wrapper = solrj_wrapper
+    @sus = solrj_wrapper.streaming_update_server
+  end
+  
+  # Given the full path to a file containing course reserve data, 
+  #   1)  remove stale course reserve information, including Solr docs that no longer have any course reserve info
+  #   2)  add the course reserve data from the file to the correct Solr documents and update the index
+  # @param crez_data_file  full path to the course reserve data file, a csv file created from Sirsi Symphony by a script written by Darsi
+  def index_crez_data(crez_data_file)
     @logger.info("Starting Course Reserve Processing")
     ckey_2_crez_info = get_ckey_2_crez_info(crez_data_file)
-    sus = solrj_wrapper.streaming_update_server
+
     @logger.info("Starting Course Reserve Indexing")
-    ac2sd = AddCrezToSolrDoc.new(ckey_2_crez_info, solrmarc_wrapper, solrj_wrapper)
-    ckey_2_crez_info.keys.each { |ckey|  
-      solr_input_doc = ac2sd.add_crez_info_to_solr_doc(ckey)
-      unless solr_input_doc.nil?
-        begin
-          sus.add(solr_input_doc)
-          @logger.info("updating Solr document #{ckey}")        
-        rescue org.apache.solr.common.SolrException => e 
-          @logger.error("SolrException while indexing document #{ckey}")
-          @logger.error("#{e.message}")
-          @logger.error("#{e.backtrace}")
-        end
-      end
-    }
+
+    @logger.info("Removing Stale Course Reserve Data")
+    prev_ckeys = get_crez_ckeys_from_index
+    remove_stale_crez_data(prev_ckeys, ckey_2_crez_info.keys)
+
+    @logger.info("Adding Course Reserve Data")
+    add_crez_data(ckey_2_crez_info)
+
     @logger.info("Starting Course Reserve Commit")
-    # commit
-    begin
-      update_response = sus.commit
-    rescue org.apache.solr.common.SolrException => e
-      @logger.error("SolrException while committing updates")
-      @logger.error("#{e.message}")
-      @logger.error("#{e.backtrace}")
-    end
+    send_ix_commit
     @logger.info("Ending Course Reserve Data Processing")
   end
   
@@ -51,20 +44,48 @@ class IndexCrezData
   # @param num_to_return - the number of ids to get.  defaults to 4500, 
   #  which is more than all the docs with crez info for a given term (roughly 3800, in general)
   # @return an array containing ids of Solr documents that have crez info
-  def get_current_crez_ckeys(solrj_wrapper, num_to_return=4500)
+  def get_crez_ckeys_from_index(num_to_return=4500)
     q = org.apache.solr.client.solrj.SolrQuery.new
     q.setQuery("access_facet:Course*")
     q.setParam("qt","search_local_params")
     q.setParam("fl", "id")
     q.setRows(num_to_return)
     q.setFacet(false)
-    doc_list = solrj_wrapper.get_query_result_docs(q)
+    doc_list = @solrj_wrapper.get_query_result_docs(q)
     current_crez_ckeys = []
     doc_list.each { |doc| 
       current_crez_ckeys << doc["id"]
     }
     current_crez_ckeys
   end
+  
+  # Remove crez data from those Solr documents that no longer have any crez data.  When a Solr doc
+  #  with crez data in the index no longer has its ckey in the crez data file from Symphony, that Solr doc
+  #  is reindexed from the marcxml only and and is rewritten to the index.
+  # @param ckeys_from_index an Array containing ids of Solr documents that have crez info in the index
+  # @param ckeys_from_data an Array containing ids of Solr documents that will be written with current crez info
+  #   Note that the current crez data file will cause all implicated solr documents to be rewritten
+  def remove_stale_crez_data(ckeys_from_index, ckeys_from_data)
+    ckeys_from_index.each { |ix_ckey|
+      if !ckeys_from_data.include?(ix_ckey)
+        sid = @solrmarc_wrapper.get_solr_input_doc_from_marcxml(ix_ckey)
+        add_solr_doc_to_ix(sid, ix_ckey)
+      end
+    }
+  end
+  
+  # Given ckeys mapped to crez data, create new SolrInputDocument objects for
+  #  each ckey that contain the fields from the marcxml and from the crez data,
+  #  and add each document to the Solr index 
+  # @param ckey_2_crez_info  Hash of ckeys mapped to Array of CSV::Row objects containing course reserve data for the ckey.
+  def add_crez_data(ckey_2_crez_info)
+    ac2sd = AddCrezToSolrDoc.new(ckey_2_crez_info, @solrmarc_wrapper, @solrj_wrapper)
+    ckey_2_crez_info.keys.each { |ckey|
+      sid = ac2sd.add_crez_info_to_solr_doc(ckey)
+      add_solr_doc_to_ix(sid, ckey)
+    }
+  end
+  
   
 protected  
   
@@ -77,4 +98,40 @@ protected
     p.ckey_2_crez_info
   end
     
+    
+  # retrieves the full marc record stored in the Solr index, runs it through SolrMarc indexing to get a SolrInputDocument that contains no course reserve information
+  #  note that it identifies Solr documents by the "id" field, and expects the marc to be stored in a Solr field "marcxml"
+  #  if there is no single document matching the id, an error is logged and nil is returned
+  # @param id  the value of the "id" Solr field for the record to be retrieved
+  def get_solr_input_doc_wo_crez(id)
+     @solrmarc_wrapper.get_solr_input_doc_from_marcxml(id)
+  end
+  
+  # add the doc to Solr by calling add on the Solrj StreamingUpdateServer object
+  # @param solr_input_doc - the SolrInputDocument to be added to the Solr index
+  # @param id - the id of the Solr document, used for log messages
+  def add_solr_doc_to_ix(solr_input_doc, id)
+    unless solr_input_doc.nil?
+      begin
+        @sus.add(solr_input_doc)
+        @logger.info("updating Solr document #{id}")        
+      rescue org.apache.solr.common.SolrException => e 
+        @logger.error("SolrException while indexing document #{id}")
+        @logger.error("#{e.message}")
+        @logger.error("#{e.backtrace}")
+      end
+    end
+  end
+  
+  # send a commit to the Solrj StreamingUpdateServer object
+  def send_ix_commit
+    begin
+      update_response = @sus.commit
+    rescue org.apache.solr.common.SolrException => e
+      @logger.error("SolrException while committing updates")
+      @logger.error("#{e.message}")
+      @logger.error("#{e.backtrace}")
+    end
+  end
+  
 end
--- spec/index_crez_data_spec.rb
+++ spec/index_crez_data_spec.rb
@@ -9,38 +9,138 @@ describe IndexCrezData do
     @solrj_wrapper = SolrjWrapper.new(@@settings.solrj_jar_dir, @@settings.solr_url, @@settings.solrj_queue_size, @@settings.solrj_num_threads)
     @sus = @solrj_wrapper.streaming_update_server
     @solr ||=  RSolr.connect :url => @@settings.solr_url
-    @index_crez_data = IndexCrezData.new
+    @index_crez_data = IndexCrezData.new(@solrmarc_wrapper, @solrj_wrapper)
   end
   
-  it "should replace the existing Solr Doc in the index with the revised document with crez info" do
-    p = ParseCrezData.new
-    crez_data_file = File.expand_path('test_data/rezdeskbldg.csv', File.dirname(__FILE__))
-    p.read(crez_data_file)
-    a = AddCrezToSolrDoc.new(p.ckey_2_crez_info, @solrmarc_wrapper, @solrj_wrapper)
-    sid_8707706_marcxml_only = a.solr_input_doc("8707706")
-    @sus.add(sid_8707706_marcxml_only)
-    @sus.commit
-    sid_8707706_b4 = get_solr_doc("8707706")
-    @index_crez_data.index_crez_data(crez_data_file, @solrmarc_wrapper, @solrj_wrapper)
-    sid_8707706_after = get_solr_doc("8707706")
-    sid_8707706_b4["crez_instructor_search"].should be_nil
-    sid_8707706_b4["crez_instructor_facet"].should be_nil
-    sid_8707706_b4["crez_display"].should be_nil
-# item_display field must be one with barcode match    
-#    item_display_val_b4 = sid_8707706_b4["item_display"].first
-#    item_display_val_b4.split("-|-").size.should == 10
-    sid_8707706_after["crez_course_info"].should_not be_nil
-    sid_8707706_after["last_updated"].should_not == sid_8707706_b4["last_updated"]
-#    item_display_val_after = sid_8707706_after["item_display"]   
-#    item_display_val_after.split("-|-").size.should == 13
+  context "get_crez_ckeys_from_index" do
+    before(:all) do
+      @crez_ckeys = @index_crez_data.get_crez_ckeys_from_index(5)
+    end
+
+    it "should return a list of Symphony ckeys" do
+      @crez_ckeys.should_not be_nil
+      @crez_ckeys[0].to_i.should be_an_instance_of(Fixnum)
+    end
+    
+    it "should only return docs with crez data" do
+      # will need to do a write to ensure this is true
+      @crez_ckeys.each { |ckey|  
+        ensure_solr_doc_has_crez_info(ckey)
+      }
+    end
+
+    it "should return ALL the docs with crez data, up to num_to_return limit" do
+      pending "to be implemented once a standalone test index is used"
+    end
   end
   
-  it "should get ckeys for existing solr docs with crez data" do
-    crez_ckeys = @index_crez_data.get_current_crez_ckeys(@solrj_wrapper, 10)
-    crez_ckeys.should_not be_nil
-    crez_ckeys[0].to_i.should be_an_instance_of(Fixnum)
+  context "remove_stale_crez_data" do
+    it "should remove crez data when the ckey no longer has crez data" do
+      i = index_crez_data_stub
+      i.should_receive(:add_solr_doc_to_ix).with do |sid_arg|
+        sid_arg["crez_course_info"].should be_nil
+      end.twice
+      ix_ckeys = ["1", "2"]
+      data_ckeys = ["3", "4"]
+      i.remove_stale_crez_data(ix_ckeys, data_ckeys)
+    end
+    
+    it "should leave solr doc alone if ckey is in current crez data" do
+      i = index_crez_data_stub
+      i.should_receive(:add_solr_doc_to_ix).with do |sid_arg|
+        sid_arg["id"].getValue.should == "1"
+      end.once
+      ix_ckeys = ["1", "2"]
+      data_ckeys = ["2", "4"]
+      i.remove_stale_crez_data(ix_ckeys, data_ckeys)
+    end
   end
 
+  context "add_crez_data" do
+    before(:all) do
+      p = ParseCrezData.new
+      p.read(File.expand_path('test_data/multfirst.csv', File.dirname(__FILE__)))
+      @ckey_2_crez_info = p.ckey_2_crez_info
+    end
+
+    it "should call add_crez_info_to_solr_doc for each ckey in the crez data" do
+      AddCrezToSolrDoc.any_instance.should_receive(:add_crez_info_to_solr_doc).with(any_args).exactly(@ckey_2_crez_info.keys.size).times
+      i = index_crez_data_stub
+      i.add_crez_data(@ckey_2_crez_info)
+    end
+
+    it "should call add_solr_doc_to_ix for each ckey in the crez data" do
+      i = index_crez_data_stub
+      i.should_receive(:add_solr_doc_to_ix).exactly(@ckey_2_crez_info.keys.size).times
+      i.add_crez_data(@ckey_2_crez_info)
+    end
+  end
+
+  context "index_crez_data" do
+    before(:all) do
+      @rezdeskbldg_data_file = File.expand_path('test_data/rezdeskbldg.csv', File.dirname(__FILE__))
+    end
+    
+    it "should call remove_stale_crez_data once" do
+      i = index_crez_data_stub
+      i.should_receive(:remove_stale_crez_data).once
+      i.index_crez_data(@rezdeskbldg_data_file)
+    end
+    
+    it "should call add_crez_data once" do
+      i = index_crez_data_stub
+      i.should_receive(:add_crez_data).once
+      i.index_crez_data(@rezdeskbldg_data_file)
+    end
+    
+    it "should call add_solr_doc_to_ix for each doc being updated" do
+      i = index_crez_data_stub
+      i.should_receive(:add_solr_doc_to_ix).exactly(10).times
+      i.index_crez_data(@rezdeskbldg_data_file)
+    end
+    
+    it "should send commit to sus at end of processing" do
+      i = index_crez_data_stub
+      i.stub(:add_solr_doc_to_ix).with(any_args)
+      i.should_receive(:send_ix_commit).once
+      i.index_crez_data(@rezdeskbldg_data_file)
+    end
+    
+    it "should update the Solr Doc with crez info and write to index (integration test)" do
+      # ensure plain doc
+      sid_8707706_b4 = get_solr_doc("8707706")
+      if !sid_8707706_b4["crez_course_info"].nil?
+        p = ParseCrezData.new
+        p.read(@rezdeskbldg_data_file)
+        a = AddCrezToSolrDoc.new(p.ckey_2_crez_info, @solrmarc_wrapper, @solrj_wrapper)
+        sid_8707706_marcxml_only = a.solr_input_doc("8707706")
+        @sus.add(sid_8707706_marcxml_only)
+        @sus.commit
+        sid_8707706_b4 = get_solr_doc("8707706")
+      end
+      sid_8707706_b4["crez_course_info"].should be_nil
+      item_disp_vals_b4 = sid_8707706_b4["item_display"].getValues
+      item_disp_vals_b4.each { |val|  
+          val.split("-|-").size.should == 10
+      }
+
+      # add crez data to index
+      @index_crez_data.stub(:remove_stale_crez_data)
+      @index_crez_data.index_crez_data(@rezdeskbldg_data_file)
+      sid_8707706_after = get_solr_doc("8707706")
+      sid_8707706_after["crez_course_info"].should_not be_nil
+      sid_8707706_after["last_updated"].should_not == sid_8707706_b4["last_updated"]
+      item_disp_vals_after = sid_8707706_after["item_display"].getValues
+      item_disp_vals_after.each { |val|
+        if val.match(/36105215224689|36105215166732/)
+          val.split("-|-").size.should == 13
+        else
+          val.split("-|-").size.should == 10
+        end
+      }
+    end
+    
+  end # index_crez_data context
 end
 
 # get the solr document object
@@ -53,3 +153,22 @@ def get_solr_doc(doc_id)
   solr_doc
 end
 
+# get the Solr document from the index using @solrj_wrapper and ensure there is course reserve information in the document
+def ensure_solr_doc_has_crez_info(doc_id)
+  q = org.apache.solr.client.solrj.SolrQuery.new
+  q.setParam("qt","document")
+  q.setParam("id", doc_id)
+  q.setParam("fl", "crez_course_info")
+  q.setFacet(false)
+  doc_list = @solrj_wrapper.get_query_result_docs(q)
+  doc_list[0]["crez_course_info"].should_not be_nil
+end  
+
+# return an IndexCrezData object with the Solr update methods stubbed
+def index_crez_data_stub
+  i = IndexCrezData.new(@solrmarc_wrapper, @solrj_wrapper)
+  i.stub(:get_crez_ckeys_from_index).with(any_args).and_return(["1", "2"])
+  i.stub(:add_solr_doc_to_ix).with(any_args)
+  i.stub(:send_ix_commit)
+  return i
+end
