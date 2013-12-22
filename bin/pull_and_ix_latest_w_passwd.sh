7527acbe8d110f1295d33503e724b6bef5f7ef7d
sw-solr-sirsi-test blacklight <blacklight@sw-solr-sirsi-test.stanford.edu>
2013-12-22 12:57:18 -0800

    change scripts for morison

--- bin/pull_and_ix_latest_w_passwd.sh
+++ bin/pull_and_ix_latest_w_passwd.sh
@@ -1,16 +1,16 @@
 #! /bin/bash
 # pull_and_ix_latest_w_passwd
-# Pull the latest course reserve data file from jenson with password prompts
+# pull the latest course reserve data file from morison with password prompts
 #  and index that file
-# If we already have the latest file on jenson, do nothing.
+# If we already have the latest file on morison, do nothing.
 # Naomi Dushay 2012-03-23
 
-REMOTE_DATA_DIR=/s/Dataload/SearchworksReserves/Data
+REMOTE_DATA_DIR=/s/SUL/Dataload/SearchworksReserves/Data
 
 #LOCAL_DATA_DIR=/data/sirsi/crez
 LOCAL_DATA_DIR=../data
 
-FULL_REMOTE_FILE_PATH="$(ssh apache@jenson ls -t $REMOTE_DATA_DIR/reserves-data.* | head -1)"
+FULL_REMOTE_FILE_PATH="$(ssh sirsi@morison ls -t $REMOTE_DATA_DIR/reserves-data.* | head -1)"
 FILE="$(basename $FULL_REMOTE_FILE_PATH)"
 
 if [ -r $LOCAL_DATA_DIR/$FILE ]; 
@@ -18,7 +18,7 @@ then
   echo "already have latest data: $FILE"
   exit 1
 else
-  scp -p apache@jenson:$FULL_REMOTE_FILE_PATH $LOCAL_DATA_DIR	
+  scp -p sirsi@morison:$FULL_REMOTE_FILE_PATH $LOCAL_DATA_DIR	
   ruby bin/crez-sw-ingest $* $LOCAL_DATA_DIR/$FILE &>$LOCAL_DATA_DIR/logs/$FILE.log
   exit 0
 fi
