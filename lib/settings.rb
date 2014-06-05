493f39c4878cd645d242314430cd4f6836159a50
Naomi Dushay <ndushay@stanford.edu>
2014-06-04 17:10:12 -0700

    add solr_source_url to settings.rb

--- lib/settings.rb
+++ lib/settings.rb
@@ -3,13 +3,16 @@ require 'yaml'
 # Read the .yml file containing the configuration values
 class Settings
   
-  attr_reader :solrmarc_dist_dir, :solrmarc_conf_props_file, :solr_url, :solrj_jar_dir, :solrj_queue_size, :solrj_num_threads, :lucene_req_handler
+  # FIXME:  can solrj_jar_dir go away?
   
+  attr_reader :solrmarc_dist_dir, :solrmarc_conf_props_file, :solr_url, :solrj_jar_dir, :solrj_queue_size, :solrj_num_threads, :lucene_req_handler, :solr_source_url
+   
   def initialize(settings_group)
     yml = YAML.load_file('config/settings.yml')[settings_group]
     @solrmarc_dist_dir = yml["solrmarc_dist_dir"]
     @solrmarc_conf_props_file = yml["solrmarc_conf_props_file"]
     @solr_url = yml["solr_url"]
+    @solr_source_url = yml["solr_source_url"]
     @lucene_req_handler = yml["lucene_req_handler"]
     @solrj_jar_dir = yml["solrj_jar_dir"]
     @solrj_queue_size = yml["solrj_queue_size"]
